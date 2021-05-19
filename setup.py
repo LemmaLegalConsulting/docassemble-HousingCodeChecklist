fa644610c89cca9833b8919bfac68bfe0758bd93
System Administrator <qsteenhuis@gmail.com>
2021-05-18 23:09:59 -0400

    Added an intro screen and use the new collapse_template function

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(name='docassemble.HousingCodeChecklist',
       url='https://courtformsonline.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.ALMassachusetts>=0.0.7', 'docassemble.AssemblyLine>=2.0.18', 'docassemble.MassAccess>=0.0.3.1'],
+      install_requires=['docassemble.ALMassachusetts>=0.0.7', 'docassemble.AssemblyLine>=2.0.19', 'docassemble.MassAccess>=0.0.3.1'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/HousingCodeChecklist/', package='docassemble.HousingCodeChecklist'),
      )
