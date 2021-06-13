70eab4b443b0dc668158ca0737324c0592d95be4
System Administrator <qsteenhuis@gmail.com>
2021-06-13 10:29:16 -0400

    Add missing files to package

---
+++ docassemble/HousingCodeChecklist/data/static/back_button.js
@@ -0,0 +1,4 @@
+$(document).on('daPageLoad', function(){
+      $(".da-field-buttons > div > .btn-da-custom").last().after("<br>")
+      $(".da-field-buttons > div > .btn-da-custom").first().before("<br>")
+});
